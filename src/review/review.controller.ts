7e30056e7c3095141c64c114d65c99ac8c0be9a1
aralon2843 <aralon2843@gmail.com>
2022-01-02 00:28:16 +0300

    feat(review): implemented service and controller

--- src/review/review.controller.ts
+++ src/review/review.controller.ts
@@ -1,14 +1,37 @@
-import { Controller, Delete, Get, Post, Body, Param } from '@nestjs/common';
-import { ReviewModel } from './review.model';
+import { REVIEW_NOT_FOUND } from './review.constants';
+import {
+  Controller,
+  Delete,
+  Get,
+  Post,
+  Body,
+  Param,
+  HttpException,
+  HttpStatus,
+} from '@nestjs/common';
+import { CreateReviewDto } from './dto/create-review.dto';
+import { ReviewService } from './review.service';
 
 @Controller('review')
 export class ReviewController {
+  constructor(private readonly reviewService: ReviewService) {}
+
   @Post('create')
-  async create(@Body() dto: Omit<ReviewModel, '_id'>) {}
+  async create(@Body() dto: CreateReviewDto) {
+    this.reviewService.create(dto);
+  }
 
   @Get('byProduct/:productId')
-  async getByProduct(@Param('productId') productId: string) {}
+  async getByProduct(@Param('productId') productId: string) {
+    this.reviewService.findByProductId(productId);
+  }
 
   @Delete(':id')
-  async delete(@Param('id') id: string) {}
+  async delete(@Param('id') id: string) {
+    const deletedDocument = await this.reviewService.delete(id);
+
+    if (!deletedDocument) {
+      throw new HttpException(REVIEW_NOT_FOUND, HttpStatus.NOT_FOUND);
+    }
+  }
 }
